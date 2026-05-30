c578570481f7937d641a4140dec3d80fef918fb6
Siaoen <104399210@students.swinburne.edu.my>
2026-05-30 20:32:15 +0800

    ..

--- frontend/KCH Bites/src/App.jsx
+++ frontend/KCH Bites/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MainPage from "./pages/MainPage";
